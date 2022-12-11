6c8e37797eae580225ac69aab6c6d976c5b195de
ansu-kri <ansukummari@gmail.com>
2022-12-11 23:36:26 +0530

    add

--- minicalci/src/App.js
+++ minicalci/src/App.js
@@ -3,13 +3,18 @@ import {add,sub,multi,div} from "./Components/Calci"
 
 
 function App() {
+  const number={
+    fontSize:"40px",
+    marginLeft:"600px",
+    marginTop:"300px"
+  }
   return (
    <>
-   <ol>
-    <li>Sum of Two no is {add(10,10)}</li>
-    <li>sub of Two no is {sub(10,10)}</li>
-    <li>multi of Two no is {multi(10,10)}</li>
-    <li>div of Two no is {div(10,3)}</li>
+   <ol type="A" className="number" style={number}>
+    <li>Sum of Two no is ={add(10,10)}</li>
+    <li>sub of Two no is ={sub(10,10)}</li>
+    <li>multi of Two no is ={multi(10,10)}</li>
+    <li>div of Two no is ={div(10,3)}</li>
     
    </ol>
    </>
