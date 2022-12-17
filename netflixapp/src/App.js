8a28c33fa4a6def298590f91fa2538fddfb82b2c
ansu-kri <ansukummari@gmail.com>
2022-12-18 00:35:02 +0530

    adding

---
+++ netflixapp/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+function App() {
+  const topnav={
+    width:"100%",
+    height:"70px",
+    backgroundColor:"black"
+  }
+  return (
+    <div className="topnav" style={topnav}>
+            <div>
+                <a href="#">
+                    <img src="https://www.edigitalagency.com.au/wp-content/uploads/Netflix-logo-red-black-png.png" width={100} />
+                </a>
+                
+            </div>
+
+        </div>
+  );
+}
+
+export default App;
---
+++ netflixapp/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />,
+    
+);
+
+// ReactDOM.render(){
+//     <>
+//     <h1>hello</h1>
+//     </>,
+//     document.getElementById("root")
+// }
+
+
