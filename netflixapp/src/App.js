f17fa535da06a9ff4e4a269122f49c74793a7656
ansu-kri <ansukummari@gmail.com>
2022-12-21 00:14:24 +0530

    add

--- netflixapp/src/App.js
+++ netflixapp/src/App.js
@@ -8,8 +8,17 @@ function App() {
     <div className="App">
       
       <Navbar/>
-      <div>
-        hello world
+      <div className="cards">
+        <div className="card">
+          <img src="https://rukminim1.flixcart.com/image/416/416/kvlaaa80/poster/d/t/u/medium-dark-netflix-series-matte-finish-poster-postrwndr6202-original-imag8g9669uvygwv.jpeg?q=70" alt="" className="card_img" />
+          <div className="card_info">
+            <span className="card_Category">A Netflix Original Series</span>
+            <h3 className="card_title">DARK</h3>
+            <a href="" target="_blank">
+              <button>Watch Now</button>
+            </a>
+          </div>
+        </div>
       </div>
       <Footer/>
     </div>
