d55b5bb8b591ccb2540aed3a087e421436d1142d
ansu-kri <ansukummari@gmail.com>
2023-01-05 18:26:52 +0530

    add count

--- myapp3/src/App.js
+++ myapp3/src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+  const state=useState();
+// console.log(state)
+
+const [count, setCount]=useState(0)
+
+// let count=1;
+
+const IncNum=()=>{
+  setCount(count+1)
+  // console.log("clicked" + count++)
+}
   return (
     <div className="App">
-      <h1>HAPPY NEW YEAR 2023</h1>
+      <h1>{count}</h1>
+      <button onClick={IncNum}>Click Me</button>
     </div>
   );
 }
